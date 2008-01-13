092e76d01caa04033d28cbd711a693e829226a11
Simon Stiefel <simon@stiefels.net>
2008-01-13 20:04:58 +0000

    initial import

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Capture Reals
+//
+//  Created by Simon Stiefel on 13.01.08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
